ba4ae25e890caeb80ecc776f1038e57a3e685198
MOHAMED <MOHAEBES@GMAIL.COM>
2024-10-02 22:07:45 +0300

    ApiCreated

--- Ecommerce/Ecommerce.api/Program.cs
+++ Ecommerce/Ecommerce.api/Program.cs
@@ -9,14 +9,10 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDbContext<DBContextApplication>(options =>
      options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
-// Add services to the container.
-builder.Services.AddScoped<ICategoryRepository, CategoryRepository>();
-builder.Services.AddScoped<ICategoryService, CategoryService>();
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
--- Ecommerce/Ecommerce.application/Repository/IRepository.cs
+++ Ecommerce/Ecommerce.application/Repository/IRepository.cs
@@ -9,8 +9,6 @@ namespace Ecommerce.application.Repository
     public interface IRepository< T> where T : class 
     {
         Task<T> GetByIdAsync(int id);
-        Task<IEnumerable<T>> GetAllAsync();
-
         Task<T>AddAsync(T entity);
         Task<T> UpdateAsync(T entity);
        Task DeleteAsync(int id);
